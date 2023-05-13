eae96e4de28753d1626f71392aa4b6d15cf7836f
gjbadros <badros@gmail.com>
2023-05-13 13:35:06 -0700

    Bump to requiring 0.0.45 for the tcp socket workarounds

--- copy-to-config.sh
+++ copy-to-config.sh
@@ -3,7 +3,7 @@
 # subdirectory and this script is run with
 # ./copy-to-config.sh
 if [ -d ../config/custom_components ]; then
-  mkdir ../config/custom_components/vantage
+  mkdir ../config/custom_components/vantage 2> /dev/null
   cp custom_components/vantage/* ../config/custom_components/vantage
 else
   echo "Directory ../config/custom_components does not exist"
